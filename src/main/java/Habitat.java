5ab3f0482df2fef358696ac74f7218e83680fe14
piotrkali <261696@student.pwr.edu.pl>
2022-05-30 21:26:10 +0200

    init

--- src/main/java/Habitat.java
+++ src/main/java/Habitat.java
@@ -38,18 +38,7 @@ public class Habitat implements Runnable{
                 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
         };
 
-        int[][] p2 = {
-                {0,1},
-                {2,3},
-                {4,5},
-                {6,7},
-                {8,9},
-                {9,8},
-                {7,6},
-                {5,3},
-                {3,2},
-                {1,0}//size 10
-        };
+
         /*
         test.add(new Individual(11, p1));
         test.add(new Individual(11, p2));
